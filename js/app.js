f33b57ab12e5a618539d96eeaa671532f17b7e1c
smit2194 <smit2194@10.0.0.218>
2020-10-29 18:38:25 -0400

    Add HTML, Javascript, and CSS files

---
+++ js/app.js
@@ -0,0 +1,64 @@
+// Create a new snippet
+$("#creator-new-snippet").click(function(e){
+
+   if($("#info-textarea").val() == ""){
+      $("#info-textarea").prop('disabled', false);
+   }
+
+   if($("#snippet-textarea").val() == ""){
+      $("#snippet-textarea").prop('disabled', false);
+   }
+
+});
+
+// Add comment
+$("#add-comment").click(function(e){
+   if($("#comment").val() == ""){
+      $("#comment").prop('disabled', false);
+   }
+});
+
+// Save comment
+$("#save-comment").click(function(e){
+   if(!$("#comment").val() == ""){
+      $("#comment").prop('disabled', true);
+   }
+});
+
+// Delete comment
+$("#delete-comment").click(function(e){
+   $("#comment").val("");
+});
+
+
+// Save info 
+$("#save-info").click(function(e){
+   if(!$("#info-textarea").val() == ""){
+      $("#info-textarea").prop('disabled', true);
+   }
+});
+
+// Save snippet
+$("#save-snippet").click(function(e){
+   if(!$("#snippet-textarea").val() == ""){
+      $("#snippet-textarea").prop('disabled', true);
+   }
+});
+
+// Edit info
+$("#edit-info").click(function(e){
+   $("#info-textarea").prop('disabled', false);
+});
+
+// Edit snippet
+$("#edit-snippet").click(function(e){
+   $("#snippet-textarea").prop('disabled', false);
+});
+
+// Delete snippet
+$("#delete-snippet").click(function(e){
+   $("#snippet-textarea").val("");
+   $("#info-textarea").val("");
+});
+
+
