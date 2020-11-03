0b114e7ca4aab9e027f341ee3f7ebb70cc3d0b95
keerthan <devikeerthan@rediff.com>
2020-11-03 18:18:05 -0500

    added random snippet id creator

--- js/app.js
+++ js/app.js
@@ -140,4 +140,13 @@ window.onload = function() {
   	loadSnippets();
 }
 
+function makeid(length) {
+   var result           = '';
+   var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   var charactersLength = characters.length;
+   for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+   }
+   return result;
+}
 
